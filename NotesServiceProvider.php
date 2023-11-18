8b9272a2a08c6e67987744033ce32f82b7ca8263
Amirmasoud <amirmasood33@gmail.com>
2023-11-17 17:35:40 -0700

    Remove spatie

--- NotesServiceProvider.php
+++ NotesServiceProvider.php
@@ -2,26 +2,35 @@
 
 namespace Coleus\Notes;
 
-use Coleus\Notes\Commands\NotesCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class NotesServiceProvider extends PackageServiceProvider
+class NotesServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register()
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('notes')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_notes_table')
-            ->hasRoute('notes')
-            ->hasCommand(NotesCommand::class)
-            ->publishesServiceProvider(NotesServiceProvider::class);
+        $this->app->bind('notes', function ($app) {
+            return new Notes();
+        });
     }
+
+    public function boot()
+    {
+        //
+    }
+    // public function configurePackage(Package $package): void
+    // {
+    //     /*
+    //      * This class is a Package Service Provider
+    //      *
+    //      * More info: https://github.com/spatie/laravel-package-tools
+    //      */
+    //     $package
+    //         ->name('notes')
+    //         ->hasConfigFile()
+    //         ->hasViews()
+    //         ->hasMigration('create_notes_table')
+    //         ->hasRoute('notes')
+    //         ->hasCommand(NotesCommand::class)
+    //         ->publishesServiceProvider(NotesServiceProvider::class);
+    // }
 }
